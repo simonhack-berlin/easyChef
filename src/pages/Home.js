8c073e3779c182dcaa100cddbde4e1be0bb35ce3
Simone Capuano <scapuano19@gmail.com>
2023-01-31 18:17:17 +0100

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Veggie from '../components/Veggie';
+import Recommended from '../components/Recommended';
+
+function Home() {
+  return (
+    <>
+        <Recommended />
+        <Veggie />
+    </>
+  )
+}
+
+export default Home;
