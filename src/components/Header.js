51fd06d0d5d9253b04465213bd246f9f51c8d385
Simone Capuano <scapuano19@gmail.com>
2023-01-31 18:18:50 +0100

    header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={classes.header}></header>
+  )
+}
+
+export default Header;
