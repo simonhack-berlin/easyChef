3367e9cbd04fcb60290e2e315a688f9ae54f6004
Simone Capuano <scapuano19@gmail.com>
2023-02-05 18:25:01 +0100

    Header component updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {GiKnifeFork} from 'react-icons/gi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 import classes from './Header.module.css';
 
 function Header(props) {
+  const {searchBar, text} = props;
+
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  }
   return (
     <header className={classes.header}>
         <Link to="/"><h1>easyCHEF <GiKnifeFork /></h1></Link>
-        <div className={classes.container}>
-          {props.children}
-        </div>
+        
+          {searchBar && <div className={classes.container}><SearchBar /></div>}
+          {text && (
+            <div className={classes.container}>
+              <h2>{text}</h2>
+              <button onClick={goBack} className={classes.button}>Go back</button>
+            </div>
+            )
+          }
     </header>
   )
 }
