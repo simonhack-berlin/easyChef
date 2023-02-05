e66efaa308b3b42f62102091381f87bc23e3e42a
Simone Capuano <scapuano19@gmail.com>
2023-02-05 18:28:55 +0100

    recipes card changed

--- src/components/Cards/Recipes.js
+++ src/components/Cards/Recipes.js
@@ -4,29 +4,49 @@ import { Link } from 'react-router-dom';
 import classes from './Recipes.module.css';
 
 function Recipes(props) {
-    const {recipes} = props;
-  return (
-    <div className={classes.container}>
-        {recipes.map((recipe) => {
-            return(
-                <Link to={`/details/${recipe.id}`} className={classes.item} key={recipe.id}>
-                    <div>
-                        <img src={recipe.image} alt={recipe.title} />
-                    </div>
-                    <h4>{recipe.title}</h4>
-                    <div className={classes.info}>
-                        <FaLeaf />
-                        <p>{recipe.vegan ? 'Vegan' : 'Not vegan'}</p>
-                    </div>
-                    <div className={classes.info}>
-                        <FaClock />
-                        <p>Ready in {recipe.readyInMinutes} minutes</p>
-                    </div>
-                </Link>   
-            );
-        })}
-    </div>
-  )
+    const {recipes, query} = props;
+
+    if (recipes) {
+        return (
+            <div className={classes.container}>
+                {recipes.map((recipe) => {
+                    return(
+                        <Link to={`/details/${recipe.id}`} className={classes.item} key={recipe.id}>
+                            <div>
+                                <img src={recipe.image} alt={recipe.title} />
+                            </div>
+                            <h4>{recipe.title}</h4>
+                            <div className={classes.info}>
+                                <FaLeaf />
+                                <p>{recipe.vegan ? 'Vegan' : 'Not vegan'}</p>
+                            </div>
+                            <div className={classes.info}>
+                                <FaClock />
+                                <p>Ready in {recipe.readyInMinutes} minutes</p>
+                            </div>
+                        </Link>   
+                    );
+                })}
+            </div>
+        )
+    }
+
+    if (query) {
+        return (
+            <div className={classes.container}>
+                {query.map((recipe) => {
+                    return(
+                        <Link to={`/details/${recipe.id}`} className={classes.item} key={recipe.id}>
+                            <div>
+                                <img src={recipe.image} alt={recipe.title} />
+                            </div>
+                            <h4>{recipe.title}</h4>
+                        </Link>   
+                    );
+                })}
+            </div>
+        )
+    }
 }
 
-export default Recipes
+export default Recipes;
