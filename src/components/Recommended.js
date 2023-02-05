c9fa7a328031771439961eee80cdc5c75a9679c7
Simone Capuano <scapuano19@gmail.com>
2023-02-05 18:30:05 +0100

    recipes number from the api changed

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -14,7 +14,7 @@ function Recommended() {
       if (check) {
         setRecommended(JSON.parse(check));
       } else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=6`);
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5`);
         const data = await api.json();
         //console.log(data);
         localStorage.setItem('recommended', JSON.stringify(data.recipes));
@@ -23,7 +23,7 @@ function Recommended() {
     }
 
   return (
-    <div className='margin'>
+    <div className='margin margin-bottom'>
         <h3>Recommended recipes</h3>
         <Recipes recipes={recommended} />
     </div>
--- src/components/Vegan.js
+++ src/components/Vegan.js
@@ -14,7 +14,7 @@ function VeganComponent() {
       if (check) {
         setVegan(JSON.parse(check));
       } else {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&tags=vegan&number=6`);
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&tags=vegan&number=5`);
         const data = await api.json();
         console.log(data);
         localStorage.setItem('vegan', JSON.stringify(data.recipes));
